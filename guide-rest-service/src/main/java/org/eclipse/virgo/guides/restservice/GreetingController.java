f8b337c774e9f558bbd7889e1e1ba58a8396bbef
Florian Waibel <fwaibel@eclipsesource.com>
2015-03-17 00:07:56 +0100

    Bug 491055 Adds recipe "Building a RESTful Web Service"

---
+++ guide-rest-service/src/main/java/org/eclipse/virgo/guides/restservice/GreetingController.java
@@ -0,0 +1,26 @@
+package org.eclipse.virgo.guides.restservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+@RequestMapping("/*")
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(value = "greeting", method = RequestMethod.GET, headers = "Accept=application/json")
+	public @ResponseBody
+	Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template,
+				name));
+	}
+
+}
